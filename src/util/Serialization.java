4f0c7c838a24fe67fe7484fe992027d3e90315f1
Valentin Couturier <valcou@aliceadsl.fr>
2013-10-09 18:18:43 +0200

    XMLToMethod et classe MethodParam

--- src/util/Serialization.java
+++ src/util/Serialization.java
@@ -1,18 +1,56 @@
 package util;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import server.API;
 
 public class Serialization {
 
 	private static final String HEX_DIGITS = "0123456789abcdef";
 	
 	// Traitement du XML
-	public static void XMLToMethod(String _xmlString) {
+	public static MethodParam XMLToMethod(String _xmlString) {
+		SAXBuilder sxb = new SAXBuilder();
+		try {
+			Document doc = sxb.build(_xmlString);
+			Element racine = doc.getRootElement();
+			
+			String nomMethode = racine.getAttributeValue("method");
+			ArrayList<Class> paramsType = new ArrayList<Class>();
+			ArrayList<String> paramsValue = new ArrayList<String>();
+			
+			for(Element param : racine.getChildren("param"))
+			{
+				paramsType.add(Class.forName(param.getAttributeValue("type")));
+				paramsValue.add(param.getText());
+			}
+			Method method = API.class.getMethod(nomMethode, (Class[]) paramsType.toArray());
+			MethodParam methodParam = new MethodParam(method, paramsValue);
+			
+			return methodParam;
+		} catch (JDOMException | IOException | ClassNotFoundException | NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
-	public static void ResultToXML(Object result)
+	public static String ResultToXML(Object result)
 	{
+		//TODO
+		Element racine = new Element("answer");
+
+		Document doc = new Document(racine);
 		
+		return null;
 	}
 	
 	
